ffef2337ef6d8330ee6ea136c63c800d2cbe375e
James <james_gower@outlook.com>
2018-05-22 13:43:42 +0100

    revered to old redirect on authroutes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -13,16 +13,20 @@ module.exports = app => {
         scope: ['email', 'public_profile']
     }));
 
-    app.get('/auth/facebook/callback', passport.authenticate('facebook'), (req, res) => {
-        res.redirect('/dashboard');
-    });
+    app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+        successRedirect: 'https://recipebox-io.herokuapp.com/dashboard',
+        failureRedirect: 'https://recipebox-io.herokuapp.com/'
+    }));
+});
 
-    app.get('/api/logout', (req, res) => {
-        req.logout();
-        res.redirect('/');
-    });
+app
+.get('/api/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
 
-    app.get('/api/current_user', (req, res) => {
-        res.send(req.user);
-    });
+app
+.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+});
 };
